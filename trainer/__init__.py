cdb3038b591a292b8dc7d21fe33ed26065ad403d
Anne Sulit <92159716+axsulit@users.noreply.github.com>
2025-05-26 13:22:41 +0800

    fix: new implementation of model

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,5 +1,11 @@
 from .abstract_trainer import AbstractTrainer, LEGAL_METRIC
-from .exp_mgpu_trainer import ExpMultiGpuTrainer
-from .exp_tester import ExpTester
-from .utils import center_print, reduce_tensor
-from .utils import exp_recons_loss
+from .single_device_trainer import SingleDeviceTrainer
+from .utils import center_print, exp_recons_loss
+
+__all__ = [
+    'AbstractTrainer',
+    'SingleDeviceTrainer',
+    'LEGAL_METRIC',
+    'center_print',
+    'exp_recons_loss'
+]
